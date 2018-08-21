a1d19744f82b724ba9476525925cc8e4e4853f95
animesh kanrewar <akanimax@gmail.com>
2018-08-21 23:28:32 +0530

    removed redundant redundancies

--- pro_gan_pytorch/__init__.py
+++
@@ -1,6 +0,0 @@
-""" Package has implementation of ProGAN (progressive growing of GANs)
-    as an extension of PyTorch Module
-"""
-import pro_gan_pytorch.PRO_GAN
-import pro_gan_pytorch.CustomLayers
-import pro_gan_pytorch.Losses
